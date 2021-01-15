4f3fea78baca9f44fbfd3765e02997f3d63cd22b
Scaffolder <scaffolder@backstage.io>
2021-01-15 15:43:09 +0000

    initial commit

---
+++ bstest3/Models/Item.cs
@@ -0,0 +1,8 @@
+namespace bstest3.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ bstest3/Properties/AssemblyInfo.cs
@@ -0,0 +1,3 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("bstest3.UnitTests")]
